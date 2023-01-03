91670e136ea323b8a6bca914092256e62a08b248
cristinamudura <cristina.mudura@nexxbiz.io>
2023-01-03 14:36:07 +0100

    Added only main projects

--- src/NexxLogic.Elsa.V2.Activities.SoapConnector/Settings/SoapActivityOptions.cs
+++
@@ -1,5 +0,0 @@
-namespace NexxLogic.Elsa.V2.Activities.SoapConnector.Settings;
-
-public class SoapActivityOptions : List<SoapServiceDefinition>
-{
-}
--- src/NexxLogic.Elsa.V2.Activities.SoapConnector/Settings/SoapServiceDefinition.cs
+++
@@ -1,6 +0,0 @@
-namespace NexxLogic.Elsa.V2.Activities.SoapConnector.Settings;
-
-public class SoapServiceDefinition
-{
-    public string WsdlEndpoint { get; set; }
-}
